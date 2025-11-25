e4d3cc4177f88dd36b045effb9f1a6674a23759f
seivarya <shivang.arya19@gmail.com>
2025-11-25 23:22:57 +0530

    circular_insert(): added

--- src/structures/list/circular/circular_list.c
+++ src/structures/list/circular/circular_list.c
@@ -1,6 +1,6 @@
-// =====================
-// | > circular_list.c |
-// =====================
+// ===================================================================================
+// | > circular_list.c | note: circular_list is based on circular doubly linked list |
+// ===================================================================================
 
 #include "circular_list.h"
 
@@ -21,7 +21,7 @@ struct circular circular_construct(void) {
 
 	struct circular list = {
 		.head = NULL,
-		.tail = NULL,            // tail shall always start equal to head
+		.tail = NULL, // tail shall always start equal to head
 		.length = 0,
 		.insert = circular_insert,
 		.remove = circular_remove,
@@ -38,8 +38,18 @@ void circular_destruct(struct circular *circular) {
 	if (!cll_validate_list(circular)) return;
 	if (!cll_validate_list_head(circular)) return;
 
-	// oh maybe i set the tail to NULL and then do the same iteration of list destructor
+	circular->tail = NULL;
+	struct circular_node *current = circular->head;
+	struct circular_node *next;
 
+	while(current != NULL) {
+		next = current->next;
+		circular_node_destruct(current);
+		current = next;
+	}
+	circular->head = NULL;
+	circular->tail = NULL;
+	circular->length = 0;
 }
 
 struct circular_node* circular_node_create(struct circular *self, void *data, size_t size) {
@@ -51,9 +61,7 @@ struct circular_node* circular_node_create(struct circular *self, void *data, si
 		perror("=== circular_node_create(): malloc failed ===\n");
 		exit(1);
 	}
-
 	*node = circular_node_construct(data, size);
-
 	return node;
 }
 
@@ -78,13 +86,33 @@ void circular_insert(struct circular *self, size_t index, void *data, size_t siz
 	struct circular_node *node_to_insert;
 	node_to_insert = circular_node_create(self, data, size);
 
+	if (!node_to_insert) return;
 	if (index == 0) {
 		if (self->length == 0) {
-
+			self->head = node_to_insert;
+			self->tail = node_to_insert;
+			node_to_insert->next = node_to_insert;
+			node_to_insert->previous = node_to_insert;
+			self->length++;
+			return;
 		}
-	}
-}
+		node_to_insert->next = self->head;
+		node_to_insert->previous = self->tail;
+		self->head->previous = node_to_insert;
+		self->tail->next = node_to_insert;
 
+		self->head = node_to_insert;
+	} else if (index == self->length) { // insertion at tail is required
+		node_to_insert->previous = self->tail;
+		self->tail->next = node_to_insert;
+		node_to_insert->next = self->head;
+		self->head->previous = node_to_insert;
+	} else {
+
+
+	}
+	self->length++;
+}
 
 void circular_remove(struct circular *self, size_t index) {
 
@@ -95,9 +123,9 @@ void circular_reverse(struct circular *self) {
 }
 
 void* circular_fetch_node(struct circular *self, size_t index) {
-
+	return NULL;
 }
 
 void* circular_fetch_data(struct circular *self, size_t index) {
-
+	return NULL;
 }
